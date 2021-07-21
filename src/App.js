eb49f734eebf4ae8086cf213ef51c9b7eb3c2781
Benjamin-Kuzava <kuzav1bj@gmail.com>
2021-07-21 15:58:49 -0400

    git good

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import logo2 from './logo2.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>You will learn about Git branches today</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://git.generalassemb.ly/sei-nyc-penguins/git-teams"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Git
         </a>
       </header>
     </div>
