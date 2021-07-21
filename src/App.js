142aaa35ee64d701287bd3ddc40a4cbf3e07fa36
Benjamin-Kuzava <kuzav1bj@gmail.com>
2021-07-21 16:15:30 -0400

    favicon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo2 from './logo2.svg';
+import logo from './logo2.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo2} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           <code>You will learn about Git branches today</code>
         </p>
